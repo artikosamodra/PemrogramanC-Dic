56519b2e078d01737d99bb099a63062f328c58f6
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-27 12:52:26 +0700

    1. Program Tanpa Kembalian Nilai

---
+++ Dasar/Program Tanpa Kembalian Nilai.c
@@ -0,0 +1,30 @@
+#include<stdio.h> /*merupakan library*/
+
+int main(void)
+
+{ 
+    printf("Halo Dunia!\n");   /*printf : untuk menampilkan intruksi yang ditulis*/
+                                /* \n : merupakan fungsi enter atau pindah baris*/
+    
+    /*aktifitas Kreatif*/
+    /*1. Gantilah program tersebut sehingga penulisnya adalah nama Anda, dan menuliskan “Hello World!.”*/
+    /*2. Contoh program yang diberikan hanya menuliskan satu baris “Halo Dunia!”.  Modifikasilah sehingga menuliskan 3 baris “Halo Dunia!.”*/
+    /*3. Tuliskan sebuah puisi di layar dengan memerintahkan program C. Kemudian telaah kembali untuk mengetahui apa bedanya dengan menuliskannya sendiri.*/
+    
+    /*1*/
+    printf("Hello Wolrd! \n");
+    
+    /*2*/
+    printf("Halo Dunia!\n");
+    printf("Halo Dunia!\n");
+    printf("Halo Dunia!\n");
+    
+    /*3*/
+    printf("Puisi Bakso\n");
+    printf("\n");
+    printf("Bakso adalah aku\n");
+    printf("Bulat kenyal dan enak\n");
+    printf("Tambah saos sambal kecap\n");
+    printf("kenyang tahan lama\n");
+
+}
