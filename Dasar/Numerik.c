0fa68d56e59e41039dd00961921f7417d0a90fe1
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-27 13:09:16 +0700

    8. Numerik

---
+++ Dasar/Numerik.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main ()
+{
+    int a;
+    float x;
+ 
+    /*printf ("Contoh membaca dan menulis, ketik nilai integer: ");
+    scanf ("%d", &a);
+    printf ("Nilai yang dibaca : %d \n", a);
+ 
+    printf ("ketik nilai sebuah bilangan riil: ");
+    scanf ("%f", &x);
+    printf ("Nilai yang dibaca : %f \n", x);*/
+ 
+/* coba ketik : scanf ("%d", a);dan tuliskan nilainya. Apa akibatnya ?*/
+/* coba ketik : scanf ("%f", x);dan tuliskan nilainya. Apa akibatnya ?*/
+/*  printf ("Contoh 2 membaca dan menulis, ketik nilai integer: ");
+    scanf ("%d", a);
+    printf ("Nilai yang dibaca : %d \n", a);
+    
+    printf ("ketik nilai sebuah bilangan riil: ");
+    scanf ("%f", x);
+    printf ("Nilai yang dibaca : %f \n", x);*/
+/*Maka muncul command : Command terminated by signal 11*/
+
+/*Aktivitas Kreatif*/
+/*
+1.  Cobalah untuk mengetikkan nilai numerik yang tidak sesuai dengan type-nya, amatilah apa yang terjadi. Dari berbagai kombinasi input yang Anda coba, simpulkan arti dari %d dan %f dengan lebih formal. Tuliskan aturan yang jelas mengenai data yang diinput dengan menggunakan format %d dan %f.
+*/
+
+/*Jawab : Maka int berubah menjadi 0  sedangkan float menjadi bilangan bulat acak (tidak ada koma)*/
+
+/*BUKTI PROGRAM*/
+/*
+    printf ("Contoh membaca dan menulis, ketik nilai integer: ");
+    scanf ("%d", &a);
+    printf ("Nilai yang dibaca : %f \n", a);
+ 
+    printf ("ketik nilai sebuah bilangan riil: ");
+    scanf ("%f", &x);
+    printf ("Nilai yang dibaca : %d \n", x);
+*/
+    
+ 
+  return 0;
+}
