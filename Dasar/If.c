3ebe896bfc6748ec448a7e6610491b51649bf1ab
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-27 13:14:51 +0700

    15. if

---
+++ Dasar/If.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{
+   int a;
+   
+   printf("Contoh IF satu kasus \n");
+   printf("ketikan suatu nilai interger \n");
+   scanf("%d", &a);
+   if (a >=0)
+   {
+       printf("Nilai a positif %d \n", a);
+   }
+   
+/*Aktivitas Kreatif*/
+/*
+1.  Gantilah program yang menulis bilangan positif dengan bilangan lebih besar dari 5.
+*/
+/*Jawab : Input 7 hasilnya 7 dan program memproses, jika nilai negatif, program tidak memproses apa-apa*/
+    return 0;
+}
