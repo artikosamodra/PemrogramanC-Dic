54db883b1dc124906877ab30cdb13b284180de1c
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-27 13:04:05 +0700

    DenganKembalian

---
+++ Dasar/DenganKembalian.c
@@ -0,0 +1,17 @@
+#include<stdio.h> /*merupakan library*/
+
+int main()
+
+{ 
+    printf("Halo Dunia!\n");   /*printf : untuk menampilkan intruksi yang ditulis*/
+                                /* \n : merupakan fungsi enter atau pindah baris*/
+    
+    /*1. Membedakan makna void main dengan int main.*/
+    /*2. Memahami konsep return dan manfaatnya walaupun belum memanfaatkannya.*/
+    
+    /*Jawab*/
+    /*1. void : hanya menampilkan data di layar || int main : data dapat diolah*/
+    /*2. return 0 : memberitahu kode telah selesai di eksekusi dengan exit code 0*/
+    
+    return 0;
+}
