075aff291654cfb8d56ee86e149dbf7c8fcbd045
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-27 13:06:57 +0700

    Assignment

---
+++ Dasar/Assignment.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ 
+int main ()
+{
+    int i;
+ 
+    i = 5; 
+    printf ("Ini nilai i : %d \n", i);
+ 
+    i = i+1;
+    printf ("Ini nilai i : %d \n", i);
+ 
+/*Aktivitas kreatif*/
+/*
+1.  Lakukan inisialisasi, dan kemudian assignment dengan nilai yang berbeda. Nilai variabel adalah nilai terakhir ketika diubah.
+*/
+ 
+    i = i+7;
+    printf("Ini nilai i : %d \n", i);
+ 
+    return 0;
+}
