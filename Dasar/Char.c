353a451af4bf555dad7ce8ac0d77a12af588a644
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-27 13:08:15 +0700

    5. Char

---
+++ Dasar/Char.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main ()
+{
+
+    char c = 65;
+    char c1;
+    char c2;/*Aktivitas kreatif*/
+
+    printf ("Karakter = %c\n", c);
+    c1 = 'Z';
+    printf ("Karakter = %c\n", c1);
+    c2 = ' ';/*Aktivitas kreatif*/
+    printf ("Karakter = %c \n", c2);/*Aktivitas kreatif*/
+ 
+
+    printf ("Karakter dalam kode ASCII = %d\n", c);
+    printf ("Karakter sebagai huruf = %c\n", c);
+    printf ("Karakter dalam kode ASCII = %d\n", c1);
+    printf ("Karakter sebagai huruf = %c\n", c1);
+    printf ("Karakter dalam kode ASCII = %d\n", c2); /*Aktivitas kreatif*/
+    printf ("Karakter sebagai huruf = %c\n", c2); /*Aktivitas kreatif*/
+ 
+/*Aktivitas kreatif*/
+/*1. “\n” artinya ganti baris. Lakukan eksplorasi di internet untuk mencari kode dari karakter “tidak kelihatan” lainnya.*/
+/*Jawab :
+    - SP (spasi) Kode 32 merupakan karakter yang tidak terlihat
+    - %d : memanggil nilai character pada tabel ASCII
+    - %c : menampilkan character atau huruf yang dideklarasikan*/
+    
+    return 0;
+}
