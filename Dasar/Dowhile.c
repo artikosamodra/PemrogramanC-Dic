e426c24a732f27977f2aca1eeb43eb7ca4f9c33a
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-27 13:13:17 +0700

    14. Dowhile

---
+++ Dasar/Dowhile.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int i=0;
+    
+    do
+    {
+        printf("Saya senang belajar C \n");
+        i++;
+    }
+    while (i<5);
+
+
+/*Aktivitas kreatif*/
+/*
+1.  Ubahlah program contoh yang diberikan dalam bagian for menjadi bentuk pengulangan yang dijelaskan pada bagian ini.
+2.  Ubahlah program contoh yang diberikan dalam bentuk while (..) {...} menjadi bentuk pengulangan yang dijelaskan dalam bagian ini.
+*/
+
+
+/*Jawab */
+/*1.*/
+/*
+    int i;
+    
+    do
+    {
+        printf("Saya senang belajar bahasa C \n");
+        i++;
+    }
+    while (i<3);
+*/
+
+/*2*/
+/*
+    int i=1;
+    
+    do
+    {
+        printf("Saya senang belajar C \n");
+        i++;
+    }
+    while (i<3);
+*/
+
+    return 0;
+}
